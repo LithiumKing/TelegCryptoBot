27ba7d64cb8f7b7d872c1e6b6063d18753005f48
Ruslan <102869689+LithiumKing@users.noreply.github.com>
2023-09-14 16:17:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+TOKEN = "6534900254:AAH9P80FihbQL8cEzdm6Um6fbdg3_cKWLck"
+
+keys = {
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "доллар": "USD",
+    "рипл": "XRP",
+    "солана": "SOL",
+    "лайткоин": "LTC"
+}
